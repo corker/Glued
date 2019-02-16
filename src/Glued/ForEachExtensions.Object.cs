a1d1d4a111037ecb3ea236afefff5eea97b63bfc
Michael Borisov <michael.borisov@franx.com>
2019-02-16 16:46:07 +0100

    simplify foreach

--- src/Glued/ForEachExtensions.Object.cs
+++ src/Glued/ForEachExtensions.Object.cs
@@ -1,33 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Glued
 {
     public static partial class ForEachExtensions
     {
-        public static IEnumerable<T>
+        public static void
             ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
-            return source.Select(t => t.Do(action));
+            foreach (var t in source) action(t);
         }
 
-        public static Func<T1, IEnumerable<T>>
+        public static Action<T1>
             ForEach<T, T1>(this IEnumerable<T> source, Action<T, T1> action)
         {
-            return t1 => source.Select(t => t.Do(action)(t1));
+            return t1 =>
+            {
+                foreach (var t in source) action(t, t1);
+            };
         }
 
-        public static Func<T1, T2, IEnumerable<T>>
+        public static Action<T1, T2>
             ForEach<T, T1, T2>(this IEnumerable<T> source, Action<T, T1, T2> action)
         {
-            return (t1, t2) => source.Select(t => t.Do(action)(t1, t2));
+            return (t1, t2) =>
+            {
+                foreach (var t in source) action(t, t1, t2);
+            };
         }
 
-        public static Func<T1, T2, T3, IEnumerable<T>>
+        public static Action<T1, T2, T3>
             ForEach<T, T1, T2, T3>(this IEnumerable<T> source, Action<T, T1, T2, T3> action)
         {
-            return (t1, t2, t3) => source.Select(t => t.Do(action)(t1, t2, t3));
+            return (t1, t2, t3) =>
+            {
+                foreach (var t in source) action(t, t1, t2, t3);
+            };
         }
     }
 }
