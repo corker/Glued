397619f36be6fccaf30a98f2a74ffc1d653f5477
Michael Borisov <michael.borisov@franx.com>
2019-02-16 12:51:03 +0100

    when with bool guard added

--- src/Glued/WhenExtensions.Object.cs
+++ src/Glued/WhenExtensions.Object.cs
@@ -5,6 +5,30 @@ namespace Glued
     public static partial class WhenExtensions
     {
         public static T
+            When<T>(this T t, bool guard, Action<T> action)
+        {
+            return guard ? t.Do(action) : t;
+        }
+
+        public static Func<T1, T>
+            When<T, T1>(this T t, bool guard, Action<T, T1> action)
+        {
+            return t1 => guard ? t.Do(action)(t1) : t;
+        }
+
+        public static Func<T1, T2, T>
+            When<T, T1, T2>(this T t, bool guard, Action<T, T1, T2> action)
+        {
+            return (t1, t2) => guard ? t.Do(action)(t1, t2) : t;
+        }
+
+        public static Func<T1, T2, T3, T>
+            When<T, T1, T2, T3>(this T t, bool guard, Action<T, T1, T2, T3> action)
+        {
+            return (t1, t2, t3) => guard ? t.Do(action)(t1, t2, t3) : t;
+        }
+
+        public static T
             When<T>(this T t, Func<T, bool> guard, Action<T> action)
         {
             return t.Do(_ =>
@@ -37,15 +61,6 @@ namespace Glued
             return (t1, t2, t3) => t.Do(_ =>
             {
                 if (guard(_)) action(_, t1, t2, t3);
-            });
-        }
-
-        public static Func<T1, T2, T3, T4, T>
-            When<T, T1, T2, T3, T4>(this T t, Func<T, bool> guard, Action<T, T1, T2, T3, T4> action)
-        {
-            return (t1, t2, t3, t4) => t.Do(_ =>
-            {
-                if (guard(_)) action(_, t1, t2, t3, t4);
             });
         }
     }
